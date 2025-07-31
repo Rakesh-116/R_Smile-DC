46605c9aede6fb826c21e7c365907b6f9e08847d
Rakesh Penugonda <119618769+Rakesh-116@users.noreply.github.com>
2025-07-31 20:40:22 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-cyan-500">
+        Welcome to My React App
+      </h1>
+    </div>
+  );
+};
+
+export default App;
