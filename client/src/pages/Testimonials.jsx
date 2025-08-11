f02ef8374315aabc1ba6aae777879fca7769c6ea
Rakesh Penugonda <119618769+Rakesh-116@users.noreply.github.com>
2025-08-11 16:19:52 +0530

    Added Testimonials Page

--- client/src/pages/Testimonials.jsx
+++ client/src/pages/Testimonials.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import TestimonialsSection from "../components/TestimonialsSection";
+import GoogleReviewsSection from "../components/GoogleReviewsSection";
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <div className="mt-30">
+      <GoogleReviewsSection />
+      <TestimonialsSection />
+    </div>
+  );
 };
 
 export default Testimonials;
